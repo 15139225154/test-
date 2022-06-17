accb45b08309f72f79cae0909a74c4546c4678f8
test_123456 <15139225154@163.com>
2022-06-17 09:31:28 +0800

    delect all file

--- pythonProject/UI_active/search_page/conftest.py
+++
@@ -1,7 +0,0 @@
-import pytest
-
-#引用login函数
-@pytest.fixture()
-def login():
-    print('123456')
-
--- pythonProject/dmoe01/test_app.py
+++
@@ -1,7 +0,0 @@
-import json
-
-import requests
-
-
-res = requests.get("https://www.baidu.com/", verify=False)
-print(res.text)
